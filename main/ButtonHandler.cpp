b1f1f9c3bdb0918dda5f07be6592cb9b89092f58
Wojciech Blachowski <wblachowsky@gmail.com>
2021-02-12 20:14:42 +0100

    Add disabling button handler upon reaching the threshold

--- main/ButtonHandler.cpp
+++ main/ButtonHandler.cpp
@@ -4,18 +4,32 @@ ButtonHandler::ButtonHandler()
 {
     lastState = 0;
     lastChange = 0L;
+    disabled = false;
 }
 
 unsigned long ButtonHandler::pressTime(int state, unsigned long now)
 {
+
     if (lastState != state)
     {
         lastState = state;
         lastChange = now;
+        disabled = false;
     }
+
+    if (disabled)
+    {
+        return 0L;
+    }
+
     if (state)
     {
         return now - lastChange;
     }
     return 0L;
+}
+
+void ButtonHandler::disableUntilChange()
+{
+    disabled = true;
 }
--- main/ButtonHandler.h
+++ main/ButtonHandler.h
@@ -3,8 +3,10 @@ class ButtonHandler
 public:
     ButtonHandler();
     unsigned long pressTime(int state, unsigned long now);
+    void disableUntilChange();
 
 private:
     int lastState;
     unsigned long lastChange;
+    bool disabled;
 };
